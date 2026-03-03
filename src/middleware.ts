570a88661c6247e0845ac85501f1b2d9afa4d120
IbrahimZahidd <232098157+MuhammadIbrahimZahid@users.noreply.github.com>
2026-03-03 18:58:07 +0500

    updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -26,8 +26,8 @@ export async function middleware(request: NextRequest) {
         getAll() {
           return request.cookies.getAll();
         },
+        // ✅ Explicit type added here
         setAll(cookiesToSet: CookieToSet[]) {
-          // ✅ add type here
           cookiesToSet.forEach(({ name, value }) =>
             request.cookies.set(name, value),
           );
